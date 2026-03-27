c16748a2c673f8291a151a2853a51169e01128bf
Wereseal <sholto.coulter@proton.me>
2026-03-27 16:54:43 +0000

    added flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "basic";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+	    jekyll
+		bundler
+      ];
+
+      shellHook = ''
+	    jekyll --version
+		bundler --version
+	    echo "Loaded into shell."
+      '';
+    };
+  };
+}
